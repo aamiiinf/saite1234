df36a7e269062c92ae951f26f96ecee9e224699a
aamiiin <aamiiinf@gmail.com>
2020-09-19 01:57:14 -0700

    modifaied fails

--- myfirstreact/app.js
+++ myfirstreact/app.js
@@ -10,16 +10,23 @@ mongoose.connect("mongodb://localhost/myTest") ;
 
 const Schema = mongoose.Schema;
 var userSchema = new Schema({
-    username : String ,
-    password : Number
+  email : String,
+  name : String,
+  password : String,
+  password_2 : String,
+  mobil : String,
+  shahr : String,
+  jensiat : String,
+  tavalod : String
 }) ;
 var InputModel = mongoose.model("User" , userSchema ) ;
 
-// const Schema2 = mongoose.Schema;
-// var oneSchema = new Schema2({
-//    onevane : String 
-// }) ;
-// var OneModel = mongoose.model("One" , oneSchema ) ;
+const Schema2 = mongoose.Schema;
+var oneSchema = new Schema2({
+   mozo : String ,
+   matn : String
+}) ;
+var OneModel = mongoose.model("One" , oneSchema ) ;
 
 app.set('views', path.join(__dirname, '/views'));
 // app.set('view engine', 'html');
@@ -42,18 +49,55 @@ app.get('/worod', function (req, res, next) {
     res.sendFile(path.join(__dirname + '/views/worod.html'));
     console.log('Page redirected to page worod.html');
 });
-app.post('/login', function (req, res, next) {
-	var formData = req.body;
-	var username = formData.username;
-	var password = formData.password;
-    
-    var newUser = new InputModel({
-    	username : formData.username , 
-    	password : formData.password
+app.post('/worod-p', function (req, res, next) {
+  var formData = req.body;
+  var email = formData.email
+  var name = formData.name
+  var password = formData.password
+  var password_2 = formData.password_2
+  var mobil = formData.mobil
+  var shahr = formData.shahr
+  var jensiat = formData.jensiat
+  var tavalod = formData.tavalod
 
-    });
-    newUser.save() ;
-    console.log(newUser);
+if(email.length && name.length && password_2.length && password.length
+  && mobil.length && shahr.length && jensiat.length && tavalod.length) {
+    if(password.length >=8 && password_2.length >=8){
+      if(password === password_2){
+        InputModel.find({email}, function (err, docs) {
+          if (err) {throw err}
+          else if(docs.length){
+              res.send('ایمیل قبلا وارد شده')
+          }else{
+            var newUser = new InputModel({
+              email : formData.email,
+              name : formData.name,
+              password : formData.password,
+              password_2 : formData.password_2,
+              mobil : formData.mobil,
+              shahr : formData.shahr,
+              jensiat : formData.jensiat,
+              tavalod : formData.tavalod
+            });
+            newUser.save() ;
+            console.log(newUser);
+            console.log('sabte name ba mofaghiat anjam shod');
+            res.send('ثبت نام با موفقیت انجام شد');
+            //res.sendFile(__dirname + '/views/new_aghahi.html')
+          }
+          
+        });
+      }else{
+        res.send('پسورد با تکرار آن مطابقت ندارد')
+      }
+     
+    }else{
+      res.send('پسورد باید 8 رقم یا بیشتر باشد')
+    }
+
+}else{
+  res.send('لطفا همه موارد را کامل کنید');
+  }
 });
 
 app.get('/modir', function (req, res, next) {
@@ -87,6 +131,53 @@ app.post('/mongos', function (req, res, next) {
           console.log(result)
         }
       });
+});
+app.post('/login-2', function (req, res, next) {
+  console.log(req.body);
+  var formData_2 = req.body;
+  var email = formData_2.email;
+  var password = formData_2.password;
+  if(email.length && password.length){
+    InputModel.find({email , password}, function (err, docs) {
+      if(docs.length){
+        res.send('خوش آمدید')
+      }else{
+          res.send('پسورد یا ایمیل اشتباه است')
+      }
+    })
+     
+  }else{
+    res.send('لطفا ایمیل و پسورد خود را وارد کنید');
+  }
+  
+});
+app.post('/darg', function (req, res, next) {
+  console.log(req.body);
+  var formData_3 = req.body;
+  var mozo = formData_3.mozo;
+  var matn = formData_3.matn;
+  if(mozo.length && matn.length){
+    var Aghahi = new OneModel({
+      mozo : formData_3.mozo,
+      matn : formData_3.matn
+    });
+    Aghahi.save() ;
+    console.log(Aghahi);
+    res.send('آگهی شما ثبت گردید و پس از تایید منتشر خواهد شد');
+     
+  }else{
+    res.send('لطفا همه موارد را کامل کنید');
+  }
+});
+app.post('/new-1', function(req, res) {
+  OneModel.find({}, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+      console.log(result)
+    }
+  });
 });
 
 app.listen(3000);
